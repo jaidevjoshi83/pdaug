c48dd459922bd9ea7934de212b2c700df37b673d
jaidevjoshi83 <jaidev53ster@gmail.com>
2019-11-11 14:36:09 -0500

    updated

--- Library_Generation_II.py
+++ Library_Generation_II.py
@@ -6,9 +6,14 @@ import os
 from itertools import permutations
 from random import shuffle
 import argparse, sys
+import pandas as pd
 
 
-def MutatedPeptides(input_file, index_list, AA):
+def MutatedPeptides(input_file, index_list, AA, output_dir):
+
+
+    if not os.path.exists(os.path.join(os.getcwd(), output_dir)):
+        os.makedirs(os.path.join(os.getcwd(), output_dir))
 
     f = open(input_file)
     line = f.readline()
@@ -51,12 +56,16 @@ def MutatedPeptides(input_file, index_list, AA):
               to_modify[int(index_list[i])-1] = replacement[i]
         counter = counter + 1
         out_put.append("".join(to_modify).upper())
-    return out_put
+
+    df = pd.DataFrame(out_put, columns=["Peptide"])
+    df.to_csv(os.path.join(output_dir,'pep.tsv'), index=False,sep='\t')
 
 
-#print MutatedPeptides('jai.txt', '1,4,5', 'A,I')
 
-def RandomPeptides(AAs, pep_length, out_pep_num):
+def RandomPeptides(AAs, pep_length, out_pep_num, output_dir):
+
+    if not os.path.exists(os.path.join(os.getcwd(), output_dir)):
+        os.makedirs(os.path.join(os.getcwd(), output_dir))
 
     if int(pep_length) > 20:
         print "Max peptide lenth 20"
@@ -78,10 +87,15 @@ def RandomPeptides(AAs, pep_length, out_pep_num):
         for i in range(int(pep_length)):
             un_seq.append(random.choice(raw))
         out_pep_lib.append("".join(un_seq))
-    return out_pep_lib
+
+    df = pd.DataFrame(out_pep_lib, columns=["Peptide"])
+    df.to_csv(os.path.join(output_dir,'pep.tsv'), index=False,sep='\t')
 
 
-def SlidingWindowPeptide(infile, window_size, frag_size):
+def SlidingWindowPeptide(infile, window_size, frag_size, output_dir):
+
+    if not os.path.exists(os.path.join(os.getcwd(), output_dir)):
+        os.makedirs(os.path.join(os.getcwd(), output_dir))
 
     if int(window_size) > 10:
         print ("Max window_size 10")
@@ -105,15 +119,12 @@ def SlidingWindowPeptide(infile, window_size, frag_size):
             pep_list.append(sequence[i*int(window_size):i*int(window_size)+int(frag_size)])
         else:
             break
-    return pep_list
 
-  
-#a = "MGSSHHHHHHSSGLVPRGSHMARVTLVLRYAARSDRGLVRANNEDSVYAGARLLALADGMGGHAAGEVASQLVIAALAHLDDDEPGGDLLAKLDAAVRAGNSAIAAQVEMEPDLEGMGTTLTAILFAGNRLGLVHIGDSRGYLLRDGELTQITKDDTFVQTLVDEGRITPEEAHSHPQRSLIMRALTGHEVEPTLTMREARAGDRYLLCSDGLSDPVSDETILEALQIPEVAESAHRLIELALRGGGPDNVTVVVADVVD"
-#AAs =ILKMFPSTWYV'
-#AA = ['A','R','N','D','B','C','E','Q','Z','G','H','I','L','K','M','F','P','S','T','W','Y','V']
-#to_modify = '--------------------'
-#s = [3,5,9,14]
-#print MutatedPeptides('jai.txt', [3,4,5,6,7] , ['A,T,Y,I'] )
+    print pep_list
+    df = pd.DataFrame(pep_list, columns=["Peptide"])
+    df.to_csv(os.path.join(output_dir,'pep.tsv'), index=False,sep='\t')
+
+
 
 if __name__=='__main__':
 
@@ -124,27 +135,30 @@ if __name__=='__main__':
     Ran.add_argument("-s","--sequence")
     Ran.add_argument("-m","--mutation_site_list")
     Ran.add_argument("-a","--AA_list")
+    Ran.add_argument("-d", "--out_dir_name", required=None, default=os.path.join(os.getcwd(),'report_dirr'),   help="Path to out file")
 
     Ran = subparsers.add_parser('RandomPeptides')
     Ran.add_argument("-a","--AA_list")
     Ran.add_argument("-l","--pep_length")
     Ran.add_argument("-o","--out_pep_lenght")
+    Ran.add_argument("-d", "--out_dir_name", required=None, default=os.path.join(os.getcwd(),'report_dirr'),   help="Path to out file")
 
     Ran = subparsers.add_parser('SlidingWindowPeptide')
     Ran.add_argument("-i","--infile")
     Ran.add_argument("-w","--winSize")
     Ran.add_argument("-s","--FragSize")
+    Ran.add_argument("-d", "--out_dir_name", required=None, default=os.path.join(os.getcwd(),'report_dirr'),   help="Path to out file")
 
     args = parser.parse_args()
 
     if sys.argv[1] == 'MutatedPeptides':
-        print MutatedPeptides(args.sequence, args.mutation_site_list, args.AA_list)
+        MutatedPeptides(args.sequence, args.mutation_site_list, args.AA_list, args.out_dir_name)
 
     elif sys.argv[1] == 'RandomPeptides':
-        print RandomPeptides(args.AA_list, args.pep_length, args.out_pep_lenght)
+        RandomPeptides(args.AA_list, args.pep_length, args.out_pep_lenght, args.out_dir_name)
 
     elif sys.argv[1] == 'SlidingWindowPeptide':
-        print SlidingWindowPeptide(args.infile, args.winSize, args.FragSize)
+        SlidingWindowPeptide(args.infile, args.winSize, args.FragSize, args.out_dir_name)
 
     else:
         print"You entered Wrong Values: "
