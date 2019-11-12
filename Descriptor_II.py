1de8efbd971ba807a84483139d4916c4d9d7efd9
jaidevjoshi83 <jaidev53ster@gmail.com>
2019-11-12 14:05:36 -0500

    update

--- Descriptor_II.py
+++ Descriptor_II.py
@@ -1,84 +1,112 @@
 from modlamp.descriptors import *
 from modlamp.sequences import *
+import os
+import pandas as pd
  
 
+def Descriptor_calcultor(DesType, inputfile, out_dir, ph, amide ):
 
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-desc = GlobalDescriptor('AFDGHLKI')
+    df = pd.read_csv(inputfile)
+    list_pep_name = df[df.columns.tolist()[0]].tolist()
+    desc = GlobalDescriptor(list_pep_name)
 
-"""
-def Descriptor_calcultor(DesType, inputfile, out_dir):
+    if DesType == "Length":
+        desc.length()
+        df = desc.descriptor
+        dfN = desc.featurenames
 
+    elif DesType == "Weight":
+        desc.calculate_MW(amide=amide)
+        df = desc.descriptor
+        dfN = desc.featurenames
 
+    elif DesType == "Charge":
+        desc.calculate_charge(ph=int(ph), amide=amide)
+        df = desc.descriptor
+        dfN = desc.featurenames
 
-	if DesType == "Length":
+    elif DesType == "ChargeDensity":
+        desc.charge_density(ph=int(ph), amide=amide)
+        df = desc.descriptor
+        dfN = desc.featurenames
 
-	elif DesType == "Formula":
+    elif DesType == "IsoelectricPoint":
+        desc.isoelectric_point()
+        df = desc.descriptor
+        dfN = desc.featurenames
+ 
+    elif DesType == "InstabilityIndex":
+        desc.instability_index()
+        df = desc.descriptor
+        dfN = desc.featurenames
 
-	elif DesType == "Weight":
+    elif DesType == "Aromaticity":
+        desc.aromaticity()
+        df = desc.descriptor
+        dfN = desc.featurenames
 
-	elif DesType == "Charge":
+    elif DesType == "AliphaticIndex":
+        desc.aliphatic_index()
+        df = desc.descriptor
+        dfN = desc.featurenames
 
-	elif DesType == "ChargeDensity":
-
-	elif DesType == "IsoelectricPoint":
-
-	elif DesType == "InstabilityIndex":
-
-	elif DesType == "Aromaticity":
-
-	elif DesType == "AliphaticIndex":
-    
     elif DesType == "BomanIndex":
+        desc.boman_index()
+        df = desc.descriptor
+        dfN = desc.featurenames
 
     elif DesType == "HydrophobicRatio":
+        desc.hydrophobic_ratio()
+        df = desc.descriptor
+        dfN = desc.featurenames
 
     elif DesType == "All":
-"""
+        desc.calculate_all(amide=amide)
+        df = desc.descriptor
+        dfN = desc.featurenames
 
+    df = desc.descriptor
+    dfN = desc.featurenames
+    dfOut = pd.DataFrame(df,columns=dfN)
 
+    dfOut.to_csv(os.path.join(out_dir,'pep_des.tsv'), index=True,sep='\t')
 
+if __name__=="__main__":
 
-desc.length()
+    seq = ['AFDGHLKI','AFDGHLKI','AFDGHLKI','AFDGHLKI']
 
-for a in desc.descriptor:
-	print (a[0])
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-t", "--DesType",
+                        required=True,
+                        default=None,
+                        help="Descriptors Type")
+                        
+    parser.add_argument("-i", "--InFile",
+                        required=True,
+                        default=None,
+                        help="Peptide File")   
 
+    parser.add_argument("-p", "--Ph",
+                        required=False,
+                        default=7.0,
+                        help="Ph 0-14") 
 
-import pandas as pd 
+    parser.add_argument("-a", "--Amide",
+                        required=False,
+                        default="True",
+                        help="True or False")   
 
-desc = GlobalDescriptor(['AFDGHLKI','AFDGHLKI','AFDGHLKI','AFDGHLKI'])
-desc.length()
+    parser.add_argument("-o", "--OutDir",
+                        required=None,
+                        default=os.path.join(os.getcwd(),'OutDir'),
+                        help="Path to out file")  
+                          
+    args = parser.parse_args()
 
-df = pd.DataFrame(desc.descriptor)
-
-print (df)
-print (desc.descriptor)
-
-desc = GlobalDescriptor('IAESFKGHIPL')
-desc.calculate_MW(amide=True)
-#print (desc.descriptor[0])
-desc.calculate_charge(ph=7.0, amide=True)
-#print ('CHARAGE',desc.descriptor[0])
-desc.charge_density(ph=6, amide=True)
-#print (desc.descriptor[0])
-desc.isoelectric_point()
-#print (desc.descriptor[0])
-desc.instability_index()
-#print (desc.descriptor[0])
-desc.aromaticity()
-#print (desc.descriptor[0])
-desc.aliphatic_index()
-#print (desc.descriptor[0])
-desc.boman_index()
-#print (desc.descriptor[0])
-desc.hydrophobic_ratio()
-#print (desc.descriptor[0])
-
-
-desc.calculate_all(amide=True)
-desc.featurenames
-['Length', 'MW', 'ChargeDensity', 'pI', 'InstabilityInd', 'Aromaticity', 'AliphaticInd', 'BomanInd', 'HydRatio']
-#print (desc.descriptor)
-
-#desc.save_descriptor('/path/to/outputfile.csv')  # save the descriptor data (with feature names header)
+    Descriptor_calcultor(args.DesType, args.InFile, args.OutDir, args.Ph, args.Amide)
