6bf7067d9ecbea919885ff3b7f0630baed2099d2
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-05-15 23:37:41 -0400

    test

--- modlamp_wetlab/modlamp_wetlab.py
+++ modlamp_wetlab/modlamp_wetlab.py
@@ -580,6 +580,7 @@ if __name__=="__main__":
             cd.dichroweb(args.CalcType)
             cd.plot(Workdirpath=args.Workdirpath, htmlOutDir=args.htmlOutDir, data=args.CalcType, combine='solvent')
             os.system('ls -lh')
+            os.system('ls Dichro/')
         elif args.CalcType == 'mean_residue_ellipticity':
             cd.calc_meanres_ellipticity()
             cd.dichroweb(args.CalcType)
