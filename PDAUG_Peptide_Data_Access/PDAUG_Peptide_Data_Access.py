1637e92f1d6fb2bac279444329ef00149e025fba
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-10-27 17:09:34 -0400

    options added

--- PDAUG_Peptide_Data_Access/PDAUG_Peptide_Data_Access.py
+++ PDAUG_Peptide_Data_Access/PDAUG_Peptide_Data_Access.py
@@ -3,10 +3,12 @@ from modlamp.datasets import load_AMPvsTM
 from modlamp.datasets import load_AMPvsUniProt
 from modlamp.datasets import load_ACPvsTM
 from modlamp.datasets import load_ACPvsRandom
+from modlamp.database import query_apd
+from modlamp.database import query_camp
 import os
 import pandas as pd
 
-def DataGen(DataBaseType, OutFile):
+def DataGen(DataBaseType, OutFile, IDs):
 
     if DataBaseType == 'AMPvsTM':
         data = load_AMPvsTM()
@@ -19,6 +21,20 @@ def DataGen(DataBaseType, OutFile):
 
     elif DataBaseType == 'ACPvsRandom':
         data = load_ACPvsRandom()
+
+    elif DataBaseType == 'query_apd':
+
+        data = query_apd([int(i) for i in IDs.split(',')])
+        df = pd.DataFrame(data, columns=['Peptides'])
+        df.to_csv(OutFile, index=False, sep='\t')
+        exit()
+
+    elif DataBaseType == 'query_camp':
+        data = query_camp([int(i) for i in IDs.split(',')])
+        df = pd.DataFrame(data, columns=['Peptides'])
+        df.to_csv(OutFile, index=False, sep='\t')
+        exit()
+
     else:
         print ("Enter Correct Values")
         exit()
@@ -52,5 +68,10 @@ if __name__=="__main__":
                         default='Out.tsv',
                         help="Out put file name for str descriptors")   
 
+    parser.add_argument("-L", "--List",
+    					required=False,
+    					default=None,
+    					help="List of integer as ID")
+
     args = parser.parse_args()
-    DataGen(args.DataBaseType, args.OutFile)
+    DataGen(args.DataBaseType, args.OutFile, args.List)
