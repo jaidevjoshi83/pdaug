d864c59024530d36b2896e261509e789fcdd20b3
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-12-28 22:23:56 -0500

    OutputData fixed

--- PDAUG_TSVtoFASTA/PDAUG_TSVtoFASTA.py
+++ PDAUG_TSVtoFASTA/PDAUG_TSVtoFASTA.py
@@ -14,17 +14,23 @@ def TSVtoFASTA(InFile, Method, Positive, Negative, OutFile):
 
         n = 0
         m = 0
+        
+        l = []
+
+        for line in lines[1:]:
+            l.append(line.split('\t')[1].strip('\n').strip('\r'))
+        l = list(set(l))
 
         for line in lines:
 
-            if '1' in line.split('\t')[1].strip('\n'):
+            if l[0] in line.split('\t')[1].strip('\n').strip('\r'):
                 n= n+1
-                of1.write('>peptide_'+str(n)+'\n')
+                of1.write('>peptide_'+str(n)+'_'+str(l[0])+'\n')
                 of1.write(line.split('\t')[0]+'\n')
 
-            if '0' in line.split('\t')[1].strip('\n'):
+            if l[1] in line.split('\t')[1].strip('\n').strip('\r'):
                 m= m+1
-                of2.write('>peptide_'+str(m)+'\n')
+                of2.write('>peptide_'+str(m)+'_'+str(l[1])+'\n')
                 of2.write(line.split('\t')[0]+'\n')
 
     elif Method == 'NoClassLabel':
@@ -47,11 +53,10 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-I", "--InFile", required=True, default=None, help=".fasta or .tsv")
-    parser.add_argument("-P", "--Postvs", required=False, default='Positive.fasta', help="Path to target tsv file")
-    parser.add_argument("-N", "--Negtvs", required=False, default='Negative.fasta', help="Path to target tsv file")
+    parser.add_argument("-P", "--Postvs", required=False, default='FirstDataFile.fasta', help="Path to target tsv file")
+    parser.add_argument("-N", "--Negtvs", required=False, default='SecondDataFile.fasta', help="Path to target tsv file")
     parser.add_argument("-O", "--OutFile", required=False, default='OutFile.fasta', help="Path to target tsv file")
     parser.add_argument("-M", "--Method", required=True, default=None, help="Path to target tsv file")
     args = parser.parse_args()
 
     TSVtoFASTA(args.InFile, args.Method, args.Postvs, args.Negtvs, args.OutFile)
-
